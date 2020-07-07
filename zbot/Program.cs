c21913eca1157a0cda339a4d10266b9bdd4ffb77
Zack E <minedogz@live.com>
2020-07-07 04:38:14 -0400

    Can click in the middle :3

--- zbot/Program.cs
+++ zbot/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Windows;
+using System.Threading;
+using System.Drawing;
+using System.Collections;
 
 namespace zbot
 {
@@ -23,11 +27,16 @@ namespace zbot
         static void Main(string[] args)
         {
             Start();
+            Thread.Sleep(1000);
             process = GetProcess();
+            Thread.Sleep(1000);
             if (process != null)
             {
                 Log("Found!");
+                Thread.Sleep(1000);
                 SetWindow(process, windowX, windowY, windowWidth, windowHeight);
+                Thread.Sleep(1000);
+                DoMouse(windowWidth / 2, windowHeight / 2);
             }
             Console.ReadLine();
         }
@@ -68,6 +77,10 @@ namespace zbot
                 return null;
             }
         }
+        static int GetProcessId()
+        {
+            return process.Id;
+        }
 
         //
         // get desktop window
@@ -104,10 +117,137 @@ namespace zbot
         //
         // Set window function
         //
+        [DllImport("User32.dll", SetLastError = true)]
+        static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
         public static void SetWindow(Process p, int x, int y, int width, int height)
         {
             IntPtr window = FindWindowByCaption(IntPtr.Zero, p.MainWindowTitle);
             SetWindowPos(window, IntPtr.Zero, x, y, width, height, 0);
+            SwitchToThisWindow(window, true);
+        }
+
+        //
+        // Fake mouse
+        //
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PointInter
+        {
+            public int X;
+            public int Y;
+            public static explicit operator Point(PointInter point) => new Point(point.X, point.Y);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out PointInter lpPoint);
+
+        // For your convenience
+        public static Point GetCursorPosition()
+        {
+            PointInter lpPoint;
+            GetCursorPos(out lpPoint);
+            return (Point)lpPoint;
+        }
+
+        public static void MoveCursorToPoint(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+
+        private static void DoMouse(int x, int y)
+        {
+            
+            Point target = new Point(x, y);
+            Point point = GetCursorPosition();
+            
+
+            int range = 10;
+            bool inside = false;
+
+            if (target.X >= point.X - range && target.X <= point.X + range)
+            {
+                if (target.Y >= point.Y - range && target.Y <= point.Y + range)
+                {
+                    Log("In Range");
+                    inside = true;
+                }
+            }
+
+
+            if (inside)
+            {
+                LeftClick();
+            }
+            else
+            {
+                Log("Moving to location");
+                Point next = Lerpz(point, target, 0.05);
+                MoveCursorToPoint(next.X, next.Y);
+                Thread.Sleep(10);
+                Draw(Brushes.Red, point.X, point.Y, 10, 10);
+                Draw(Brushes.Blue, target.X, target.Y, 10, 10);
+                DoMouse(x, y);
+            }
+           
+
+        }
+
+        private static int Lerp(int firstFloat, int secondFloat, double by)
+        {
+            return Convert.ToInt32(firstFloat * (1 - by) + secondFloat * by);
+        }
+        private static Point Lerpz(Point firstVector, Point secondVector, double by)
+        {
+            int retX = Lerp(firstVector.X, secondVector.X, by);
+            int retY = Lerp(firstVector.Y, secondVector.Y, by);
+            return new Point(retX, retY);
+        }
+
+
+        //
+        // Mouse click events
+        //
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+        public static void LeftClick()
+        {
+            mouse_event((int)(MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            mouse_event((int)(MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+        }
+
+
+        //
+        // Draw
+        //
+        [DllImport("User32.dll")]
+        static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("User32.dll")]
+        static extern int ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        static void Draw(Brush brush, int x, int y, int w, int h)
+        {
+            IntPtr desktop = GetDC(IntPtr.Zero);
+            using (Graphics g = Graphics.FromHdc(desktop))
+            {
+                ArrayList al = new ArrayList();
+                g.FillEllipse(brush, Convert.ToInt32(x * 1.5), Convert.ToInt32(y * 1.5), w, h);
+            }
+            ReleaseDC(IntPtr.Zero, desktop);
         }
     }
 }
